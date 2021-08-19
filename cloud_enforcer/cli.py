8bbf6260113b3363b8b4ccc1a31a1c61b1bb82d9
Dimitry Dukhovny <dimitry@dukhovny.net>
2021-08-19 23:23:06 +0100

    Switching to a single conf dictionary

--- cloud_enforcer/cli.py
+++ cloud_enforcer/cli.py
@@ -1,23 +1,64 @@
 """Console script for cloud_enforcer."""
 import sys
+import os
 import click
+
 try:
     import cloud_enforcer.cloud_enforcer as ce
 except:
     import cloud_enforcer as ce
 
+import colorama
+
+def OUT(outstring=''):
+    '''
+    Simple output printer.
+    Will halt the program if it fails to write to stdout.
+
+    Args:
+        outstring: a string or anything that can become one
+    Returns:
+        None
+    '''
+    try:
+        outstring = str(outstring)
+    except:
+        outstring = ''
+    #sys.stdout.write(outstring + '\n')
+    print(colorama.Fore.GREEN + outstring + colorama.Style.RESET_ALL, file=sys.stdout, flush=True)
+
+def ERR(outstring=''):
+    '''
+    Simple error printer.
+    Will halt the program if it fails to write to stderr.
+
+    Args:
+        outstring: a string or anything that can become one
+    Returns:
+        None
+    '''
+    try:
+        outstring = str(outstring)
+    except:
+        outstring = ''
+    #sys.stderr.write(outstring + '\n')
+    print(colorama.Fore.RED + outstring + colorama.Style.RESET_ALL, file=sys.stderr, flush=True)
+
+
 
 @click.command()
-@click.option('--reinstall', '-r',
-    type=click.BOOL, default=False,
+@click.option('--reinstall', '-r', type=click.BOOL,
+    default=False,
     help='Force PYPI dependency reinstallation in user namespace.')
 @click.option('--config', '-c', default='cloud_enforcer.yml',
     help='Specify a YAML-formatted config file')
+@click.option('--ansible_tree', '-t', default="%s/ansible" % os.getenv('HOME'),
+    help='Ansible tree location, ideally with an ansible.cfg .')
 @click.option('--ansible_user', '-u', default='root',
     help='Remote user name.')
-@click.option('--ssh_key_path', '-s', type=click.PATH, default='.ssh/id_rsa',
+@click.option('--ssh_key_path', '-s', type=click.Path(), default='',
     help='SSH key location.')
-@click.option('--password_file', '-f', type=click.PATH, default='',
+@click.option('--password_file', '-f', type=click.Path(), default='',
     help='Password file location.')
 @click.option('--password_prompt', '-P', type=click.BOOL, default=False,
     help='Prompt for a password.')
@@ -27,9 +68,11 @@ except:
     help='Host or group run limit.')
 @click.option('--boot', '-n', type=click.BOOL, default=False,
     help='Allow reboots.')
+@click.argument('action')
 
 def main(args=None,
     config='',
+    ansible_tree='',
     ansible_user='',
     ssh_key_path='',
     password_file='',
@@ -49,7 +92,8 @@ def main(args=None,
         password_prompt=password_prompt,
         inventory_limit=inventory_limit,
         playbook=playbook,
-        boot=boot)
+        boot=boot,
+        action=action)
     return 0
 
 
--- cloud_enforcer/cloud_enforcer.py
+++ cloud_enforcer/cloud_enforcer.py
@@ -1,7 +1,43 @@
 """Main module."""
 import pip
 import yaml
-import ansible.runner
+import ansible_runner
+import colorama
+
+def OUT(outstring=''):
+    '''
+    Simple output printer.
+    Will halt the program if it fails to write to stdout.
+
+    Args:
+        outstring: a string or anything that can become one
+    Returns:
+        None
+    '''
+    try:
+        outstring = str(outstring)
+    except:
+        outstring = ''
+    #sys.stdout.write(outstring + '\n')
+    print(colorama.Fore.GREEN + outstring + colorama.Style.RESET_ALL, file=sys.stdout, flush=True)
+
+def ERR(outstring=''):
+    '''
+    Simple error printer.
+    Will halt the program if it fails to write to stderr.
+
+    Args:
+        outstring: a string or anything that can become one
+    Returns:
+        None
+    '''
+    try:
+        outstring = str(outstring)
+    except:
+        outstring = ''
+    #sys.stderr.write(outstring + '\n')
+    print(colorama.Fore.RED + outstring + colorama.Style.RESET_ALL, file=sys.stderr, flush=True)
+
 
 class LocalConfig:
     """
@@ -10,7 +46,7 @@ class LocalConfig:
     def __init__(self, **entries):
         self.__dict__.update(entries)
 
-def doconfig():
+def doconfig(config):
     """
     Return config object from YAML file read with '-c' parameter.
     """
@@ -22,12 +58,29 @@ def doconfig():
     conf = LocalConfig(**configmap)
     return(conf)
 
-def enforce():
+def saveconfig(config):
+    """
+    Save config as entered on the commandline
+    """
+    pass
+    
+
+def enforce(conf):
     """
     Run Ansible playbook with specified parameters.
     """
-    
-
+    play = ansible.runner.run(
+        conf.playbook='',
+        conf.inventory='',
+        conf.host_pattern='all',
+        conf.extravars=extravars)
+    OUT("{}: {}".format(play.status, play.rc))
+    # successful: 0
+    for each_host_event in play.events:
+        OUT(each_host_event['event'])
+    print("Final status:")
+    print(play.stats)
+    return(play)
 
 def main(args=None,
     config='',
@@ -37,12 +90,46 @@ def main(args=None,
     password_prompt=False,
     inventory_limit='',
     playbook='',
-    boot=False):
+    boot=False,
+    argument='play'):
     """
     Replaces previous execution with Makefiles.
     """
+    os.environ['ANSIBLE_CONFIG'] = "%s/ansible.cfg" % (ansible_tree)
+    extravars = {}
     conf = doconfig()
-    
+    conf.ansible_user = ansible_user
+    conf.ssh_key_path = ssh_key_path
+    conf.password_file = password_file
+    conf.password_prompt = password_prompt
+    conf.inventory_limit = inventory_limit
+    conf.playbook=conf.playbook,
+    conf.inventory="%s/inventory" % (ansible_tree)
+    conf.host_pattern=inventory_limit
+    conf.extravars=extravars
+    conf.extravars['accept_boot'] = boot
+
+    if conf.password_file:
+        try:
+            with open(conf.password_file) as FH:
+                conf.extravars['ansible_password'] = FH.read()
+        except:
+            ERR('Could not read %s' % (str(conf.password_file)))
+
+    if conf.password_prompt:
+        try:
+            conf.extravars['ansible_password'] = getpass(
+                "Password for %s: " % (conf.ansible_user))
+        except:
+            ERR('Could not parse user entry for password.')
+
+    conf.extravars['ansible_sudo_password'] = conf.extravars['ansible_password']
+    if argument == 'play':
+        enforce(conf)
+    elif argument == 'save':
+        saveconfig(conf)
+    else:
+        ERR('Options are "play" or "save ," not "%s .' % (argument))
 
 
 if __name__ == "__main__":
